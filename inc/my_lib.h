ba0291156329d482706dd7856c9fbd4f33c290c8
Nguyen Cong Ly <ly1122009@gmail.com>
2024-04-02 11:22:31 +0700

    create makefile sample

---
+++ inc/my_lib.h
@@ -0,0 +1,8 @@
+#ifndef _MY_LIB_H_
+#define _MY_LIB_H_
+#pragma once
+
+void print_HelloWorld(void);
+
+#endif
+
---
+++ src/my_lib.cc
@@ -0,0 +1,7 @@
+#include<iostream>
+#include "my_lib.h"
+
+void print_HelloWorld(void)
+{
+    std::cout << "Hello World!" << std::endl;
+}
